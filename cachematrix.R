86c6205a331ba5f8b5cf793e6f42fd491d329c60
Dadaela <53258322+Dadaela@users.noreply.github.com>
2019-07-31 15:25:06 +0200

    cachematrix_agg2.r

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,26 @@
 
 ## Write a short comment describing this function
 #Prova Daniela
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function( da = matrix() ) {
+    inversa <- NULL
+    # set the matrix
+    set <- function( matrix ) {
+            da <<- matrix
+            inversa <<- NULL
+    }
+    # get the matrix
+    get <- function() {
+    	da
+    }
+    setInverse <- function(inverse) {
+        inversa <<- inverse
+    }
+    getInverse <- function() {
+        inversa
+    }
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
@@ -12,4 +30,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  da <- x$getInverse()
+  if( !is.null(da) ) {
+    message("getting cached inverse")
+    return(da)
+  }
+  
+  data <- x$get()            ## Get x
+  da <- solve(data,...)  ##Solve 
+  x$setInverse(da)       ## Set the inverse 
+  da                     ## Return the matrix
 }
+
